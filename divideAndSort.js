bcd0cb6c6947e92a1fc92acd8050cbdeb8dfdd59
Zainul Muhammad Rofa <113880540+zainulrofa@users.noreply.github.com>
2022-10-04 14:56:33 +0700

    Update divideAndSort.js

--- divideAndSort.js
+++ divideAndSort.js
@@ -1,19 +1,10 @@
 const divideAndShort = (number) => {
-    if (typeof number !== "number") return console.log("Data Invalid")
+    // validasi
+    if (typeof number !== "number") return "Data Invalid"
 
-//     let arr = number.toString().split('')
-//     let arrNum = []
-//     for (let i = 0; i < arr.length; i++) {
-//         arrNum.push(parseInt(arr[i]))
-//     }
-//     arrNum.sort((a, b) => {
-//         return a - b
-//     })
-//     return console.log(arrNum)
-    
-     return console.log(
-        number.toString().split('0').map((el) => el.split('').sort().join('')).join('')
-    )
+    return parseInt(number.toString().split('0').map((el) => el.split('').sort().join('')).join(''))
+
 }
 
-divideAndShort(5956560159466056)
+console.log(divideAndShort(5956560159466056))
+console.log(divideAndShort('5956560159466056'))
