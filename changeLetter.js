25f5725ddf607be18b25ed96ee6a47effc4d98ea
Zainul Muhammad Rofa <113880540+zainulrofa@users.noreply.github.com>
2022-10-04 14:54:36 +0700

    Update changeLetter.js

--- changeLetter.js
+++ changeLetter.js
@@ -1,16 +1,15 @@
 const changeLetter = (word) => {
-    if (typeof word !== "string") return console.log('invalid data')
+    if (typeof word !== "string") return 'invalid data'
 
     let print = ""
-    for (let i = 0; i <= word.length; i++) {
+    for (let i = 0; i <= word.length - 1; i++) {
         if (word[i] === 'a') {
-            print = print + "o"
-        } else {
-            print = print + word[i]
+            print += "o"
+            continue
         }
+        print += word[i]
     }
-    return console.log(print)
-    // console.log(newWord = word.replace(/a/g, 'o'))
+    return (print)
 }
 
-changeLetter('jakarta')
+console.log(changeLetter('jakarta'))
