58bc9c3647f6188bc013575440641b84393401fd
Zainul Muhammad Rofa <113880540+zainulrofa@users.noreply.github.com>
2022-10-04 14:54:59 +0700

    Update palidrome.js

--- palidrome.js
+++ palidrome.js
@@ -1,10 +1,11 @@
 const cekPalindrome = (kata) => {
-    if (typeof kata !== 'string') return console.log('Data Invalid')
+    if (typeof kata !== 'string') return 'Data Invalid'
 
-    for (let i = 0; i < kata.length / 2; i++) {
-        if (kata[i] !== kata[kata.length - 1 - i]) return console.log('is not Palindrome')
+    for (let i = 0; i <= kata.length / 2; i++) {
+        if (kata[i] !== kata[kata.length - 1 - i]) return 'is not Palindrome'
     }
-    return console.log('Palindrome')
+    return ('Palindrome')
 }
 
-cekPalindrome('malam')
+console.log(cekPalindrome('malam'))
+console.log(cekPalindrome('siang'))
